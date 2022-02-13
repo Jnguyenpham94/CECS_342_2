c541aebde2cc92364ba87bf72be43e3781c4b486
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-02-13 00:06:54 -0800

    testing stuff for assn 1

--- assignments_hs/assignment_1.hs
+++ assignments_hs/assignment_1.hs
@@ -1,6 +1,7 @@
-qsort :: Ord a => [a] -> [a] -- quick sort a list in Haskell
+--qsort :: Ord a => [a] -> [a] -- quick sort a list in Haskell
 
 
-msort :: Ord a => [a] -> [a] -- merge sort a list in Haskell
+--msort :: Ord a => [a] -> [a] -- merge sort a list in Haskell
 
-main :: IO () -- takes input and output but may not need it with this file?
+--main :: IO () -- takes input and output but may not need it with this file?
+doubleMe x = x + x
