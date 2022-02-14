11bae5ee55ab944eb81eee67e08627b491b3e357
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-02-13 16:09:56 -0800

    assignment_1 haskell DONE

--- assignments_hs/assignment_1.hs
+++ assignments_hs/assignment_1.hs
@@ -1,7 +1,23 @@
---qsort :: Ord a => [a] -> [a] -- quick sort a list in Haskell
+quicksort1 :: (Ord a) => [a] -> [a]
+quicksort1 [] = []
+quicksort1 (x:xs) =
+  let smallerSorted = quicksort1 [a | a <- xs, a <= x]
+      biggerSorted = quicksort1 [a | a <- xs, a > x]
+  in  smallerSorted ++ [x] ++ biggerSorted
 
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys) | x <= y    = x:merge xs (y:ys)
+                    | otherwise = y:merge (x:xs) ys
 
---msort :: Ord a => [a] -> [a] -- merge sort a list in Haskell
+msort :: Ord a => [a] -> [a] -- merge sort a list in Haskell
+msort [] = []
+msort [a] = [a]
+msort xs = merge (msort (firstHalf xs)) (msort (secondHalf xs))
+firstHalf  xs = let { n = length xs } in take (div n 2) xs
+secondHalf xs = let { n = length xs } in drop (div n 2) xs
 
---main :: IO () -- takes input and output but may not need it with this file?
-doubleMe x = x + x
+main = do
+  print (quicksort1 [5,6,8,1,0,12,-3])
+  print (msort [-25,60,-8,5,0,12,-3,0])
