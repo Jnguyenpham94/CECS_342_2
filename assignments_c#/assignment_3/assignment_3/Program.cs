fc6d7e025e5497e7b2077bd780cd4c431a4f5d49
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-03-07 21:44:23 -0800

    assignment 3 START

---
+++ assignments_c#/assignment_3/assignment_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ The following sequence of floating point numbers:
+645.41, 37.59, 76.41, 5.31, -34.23, 1.11, 1.10, 23.46, 635.47, -876.32, 467.83, 62.25
+The following sequence of people with name and age of each person. The name is a string
+and the age an integer:
+Hal, 20; Susann, 31; Dwight, 19; Kassandra, 21; Lawrence, 25; Cindy, 22; Cory, 27;
+Mac, 19; Romana, 27; Doretha, 32; Danna, 20; Zara, 23; Rosalyn, 26; Risa, 24; Benny,
+28; Juan, 33; Natalie, 25
+ */
+
+namespace assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ assignments_c/Assignment_3/assignment_3.c
@@ -0,0 +1,28 @@
+// assignment_3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+////645.41, 37.59, 76.41, 5.31, -34.23, 1.11, 1.10, 23.46, 635.47, -876.32, 467.83, 62.25
+/*The following sequence of people with nameand age of each person.The name is a string
+and the age an integer :
+Hal, 20; Susann, 31; Dwight, 19; Kassandra, 21; Lawrence, 25; Cindy, 22; Cory, 27;
+Mac, 19; Romana, 27; Doretha, 32; Danna, 20; Zara, 23; Rosalyn, 26; Risa, 24; Benny,
+28; Juan, 33; Natalie, 25
+*/
+
+#include <iostream>
+
+int main()
+{
+    
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
