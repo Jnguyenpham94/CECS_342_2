22ba9db32ab52d26958e59b378dd83a02c59b740
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-04-01 19:03:45 -0700

    need to create overload for sort template

--- assignments_c++/assignment_3/assignment_3.cpp
+++ assignments_c++/assignment_3/assignment_3.cpp
@@ -47,12 +47,19 @@ int main()
     {
         cout << i << " ";
     }
-    cout << endl;
+    cout << endl << endl;
 
     Person people[] = { Person("Hal", 20), Person("Susann", 31), Person("Dwight", 19), Person("Kassandra", 21), Person("Lawrence", 25), Person("Cindy", 22), Person("Cory", 27), Person("Mac", 19), Person("Romana", 27), Person("Doretha", 32), Person("Danna", 20), Person("Zara", 23), Person("Rosalyn", 26), Person("Risa", 24), Person("Benny", 28), Person("Juan", 33), Person("Natalie", 25) };
     int pn = sizeof(people) / sizeof(people[0]);
-    //sort(people, people + pn);
-    cout << "People name sorted:" << endl;
+    sort(people, people + pn);
+    cout << "People name sort:" << endl;
+    for (size_t i = 0; i < size(people); i++)
+    {
+        cout << people[i].getName() << " " << people[i].getAge() << endl;
+    }
+    cout << endl;
+
+    cout << "People age sort:" << endl;
     for (size_t i = 0; i < size(people); i++)
     {
         cout << people[i].getName() << " " << people[i].getAge() << endl;
