139528cbf9f98836b2841fd5fc515e6b04f8737f
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-04-16 19:31:07 -0700

    function signatures created

--- assignments_c#/assignment_4/Assn4/Program.cs
+++ assignments_c#/assignment_4/Assn4/Program.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Assn4
 {
     class Program
     {
-        static void Main(string[] args)
+        static IEnumerable<string> EnumerateFilesRecursively(string path)
+        {
+
+        }
+
+        static string FormatByteSize(long byteSize)
+        {
+
+        }
+
+        static XDocument CreateReport(IEnumerable<string> files)
+        {
+
+        }
+
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
